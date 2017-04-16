46733fd17f40681aa2b1c90628fbabaeb7a8b652
vitaliy-voropaev <vitaliy.voropaev.ua@gmail.com>
2017-04-16 14:43:33 +0300

    Fix Messenger initialize class load

--- core/components/messenger/elements/snippets/snippet.messenger.php
+++ core/components/messenger/elements/snippets/snippet.messenger.php
@@ -1,12 +1,17 @@
 <?php
 if (!$Messenger = $modx->getService('messenger', 'Messenger', $modx->getOption('messenger_core_path', null, $modx->getOption('core_path') . 'components/messenger/') . 'model/messenger/', $scriptProperties)) {
-	return 'Could not load Messenger class!';
+    return 'Could not load Messenger class!';
 }
+
+$Messenger->initialize($modx->context->key, $scriptProperties);
+
 $messages = $Messenger->messageLoad(false, false, false);
 $dialogues = $Messenger->dialogues_load(false, false);
 $dialog = $Messenger->dialog_load('', false);
+
 $output = $modx->getChunk('tpl.Messenger.outer', array(
-	'dialogues' => $dialogues,
-	'dialog' => $dialog
+    'dialogues' => $dialogues,
+    'dialog' => $dialog
 ));
+
 return $output;
